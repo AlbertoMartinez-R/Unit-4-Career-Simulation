7b76ce41415393872cb129a6c48319c4b235c599
Alberto Martinez <avmrbeto98@gmail.com>
2024-07-03 18:52:42 -0700

    added AWS DB

--- BackEnd/database/db.js
+++ BackEnd/database/db.js
@@ -1,22 +1,37 @@
 import pg from 'pg';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const { Client } = pg;
 
 const DATABASE_URL = process.env.DATABASE_URL || `postgres://localhost:5432/amazon_store`;
+const DB_HOST = process.env.DB_HOST || 'amazonstore.cra06w0aaa0a.us-east-2.rds.amazonaws.com';
+const DB_USER = process.env.DB_USER || 'postgres';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'PotatoSalad123456789';
+const DB_NAME = process.env.DB_NAME || 'amazonstore';
 
-export const client = new Client(DATABASE_URL);
-
+const client = new Client({
+    host: DB_HOST,
+    port: 5432,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('path/to/TestPurposes.pem').toString(),
+    },
+});
 
 export const connectDataBase = async () => {
     try {
         await client.connect();
-
-        console.log(`Successfully connected to the Database ${DATABASE_URL}`);
-
-    } catch(e) {
-        console.error(`Failed to connect to the Database ${DATABASE_URL}`);
+        console.log(`Successfully connected to the Database ${DB_HOST}`);
+    } catch (e) {
+        console.error(`Failed to connect to the Database ${DB_HOST}`);
         console.error(e);
-    };
+    }
 };
 
-
+export { client };
